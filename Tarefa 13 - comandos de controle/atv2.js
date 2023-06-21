26a330a78b270f45284c80ba6f9806d5cf717eb8
Beatriz Karoline <beatriz.karoline@ufpe.br>
2023-06-21 20:43:56 -0300

    ajutes

--- Tarefa 13 - comandos de controle/atv2.js
+++ Tarefa 13 - comandos de controle/atv2.js
@@ -4,10 +4,8 @@
 
 // Faça um programa JavaScript em que o usuário informará a sua idade e o programa emitirá uma mensagem(alert) dizendo se o usuário é ou não menor de idade 
 
-// Receber a idade do usuário
 var idade = parseInt(prompt("Informe a sua idade:"));
 
-// Verificar se o usuário é menor de idade
 if (idade < 18) {
   alert("Você é menor de idade.");
 } else {
--- Tarefa 13 - comandos de controle/atv3.js
+++ Tarefa 13 - comandos de controle/atv3.js
@@ -4,7 +4,6 @@
 
 // Faça um programa JavaScript em que o usuário informará a sua idade e o programa emitirá uma mensagem(alert) dizendo se o usuário é ou não menor de idade 
 
-// Encontrar a soma dos múltiplos de 3 ou 5 abaixo de 1000
 var soma = 0;
 
 for (var i = 1; i < 1000; i++) {
@@ -13,5 +12,4 @@ for (var i = 1; i < 1000; i++) {
   }
 }
 
-// Exibir o resultado em um site
 document.write("A soma dos múltiplos de 3 ou 5 abaixo de 1000 é: " + soma);
