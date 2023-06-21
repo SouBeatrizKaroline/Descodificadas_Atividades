3f6fd0f8f76447b7fa2b1c062b9894ce7e4e11bb
Beatriz Karoline <beatriz.karoline@ufpe.br>
2023-06-20 23:08:25 -0300

    Tarefa 12 - Funções

---
+++ Tarefa 12 - Funções/atv1.js
@@ -0,0 +1,11 @@
+// Faça um projeto que contenha uma função JavaScript em que o usuário informe 3 números e o programa calcule a média aritmética desses números e mostre o resultado
+
+function calcularMedia() {
+    var numero1 = parseFloat(document.getElementById("numero1").value);
+    var numero2 = parseFloat(document.getElementById("numero2").value);
+    var numero3 = parseFloat(document.getElementById("numero3").value);
+
+    var media = (numero1 + numero2 + numero3) / 3;
+
+    document.getElementById("resultado").innerHTML = "A média aritmética é: " + media;
+  }
